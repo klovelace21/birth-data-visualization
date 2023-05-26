bb42a83f3c2872d2b8508717afd432f61513a2d0
Kenneth <kennethlovelace21@gmail.com>
2023-05-26 19:20:26 -0400

    Verify repo is working

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-<<<<<<< HEAD
-#test....
-=======
-#test2
->>>>>>> a650d16 (verify fix)
+#verify working
 
 st.title("what it doooo")
