e3ac8b1f7ef81ee5eb199e6a5457939d7f4bc695
Kenneth <kennethlovelace21@gmail.com>
2023-05-30 12:32:06 -0400

    Obtains total sum of undergrad births by state

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
+from matplotlib import pyplot
 
-#verify working
+df = pd.read_csv("us_births_2016_2021.csv")
 
-st.title("what it doooo")
+# finding all parents with undergraduate and below level of education
+undergraduate = df[(df['Education Level Code'] < 5) & (df['Education Level Code'] > 0)]
+undergraduate.drop(columns=['State', 'Year', 'Gender', 'Education Level of Mother', 'Education Level Code',
+                                  'Average Age of Mother (years)', 'Average Birth Weight (g)'], axis=1, inplace=True)
+undergrad_by_state_total = undergraduate.groupby("State Abbreviation")
+print(undergrad_by_state_total.sum())
+
+
+
+
+
