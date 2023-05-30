666cc7f25dac511f0c5a1f8b4897de6013620fcf
Kenneth <kennethlovelace21@gmail.com>
2023-05-30 13:01:45 -0400

    Created display for undergrad totals by state

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ from matplotlib import pyplot
 
 df = pd.read_csv("us_births_2016_2021.csv")
 
+st.title("Visualization of Birth Data Related to Education Level")
 # finding all parents with undergraduate and below level of education
-undergraduate = df[(df['Education Level Code'] < 5) & (df['Education Level Code'] > 0)]
+undergraduate = df[(df['Education Level Code'] <= 5) & (df['Education Level Code'] > 0)]
 undergraduate.drop(columns=['State', 'Year', 'Gender', 'Education Level of Mother', 'Education Level Code',
                                   'Average Age of Mother (years)', 'Average Birth Weight (g)'], axis=1, inplace=True)
 undergrad_by_state_total = undergraduate.groupby("State Abbreviation")
-print(undergrad_by_state_total.sum())
+
+# creating streamlit display for undergrad state totals
+st.header("Total Number of Births by State")
+st.subheader("Education Level: Undergraduate and Below")
+undergrad_options = ['Most Births', 'Least Births']
+undergrad_display = st.radio('Sort by states with:', undergrad_options)
+if undergrad_display == 'Most Births':
+    st.bar_chart(undergrad_by_state_total.sum().sort_values('Number of Births').tail(15))
+else:
+    st.bar_chart(undergrad_by_state_total.sum().sort_values('Number of Births').head(15))
+
+
 
 
 
